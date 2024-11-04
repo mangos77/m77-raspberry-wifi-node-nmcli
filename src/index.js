b6e1220b2995cb864d0a96d007e0a256afa8f113
Carlos Yerena <carlos@mangos77.mx>
2024-11-04 21:10:55 +0000

    feat: add index to export both modules

---
+++ src/index.js
@@ -0,0 +1,4 @@
+const M77RaspberryWIFI = require('./m77-raspberry-wifi-node-nmcli')
+const M77RaspberryETH = require('./m77-raspberry-ethernet-node-nmcli')
+
+module.exports = { M77RaspberryWIFI, M77RaspberryETH }
